0e3c4b85a426f17eb41551a48e47810a44d9b5e5
Acciente717 <221thunder@gmail.com>
2019-12-21 22:08:34 +0800

    Add LTE_PHY_Serv_Cell_Measurement.

--- action_list.cpp
+++ action_list.cpp
@@ -56,6 +56,8 @@ static void extract_lte_phy_pdsch_stat_packet(
     pt::ptree &&tree, Job &&job);
 static void extract_lte_phy_pdsch_packet(
     pt::ptree &&tree, Job &&job);
+static void extract_lte_phy_serv_cell_measurement(
+    pt::ptree &&tree, Job &&job);
 
 /// The list storing all `ConditionalAction`s.
 ActionList g_action_list;
@@ -186,6 +188,17 @@ void initialize_action_list() {
                 );
             },
             extract_lte_phy_pdsch_packet
+        }
+    );
+
+    g_action_list.push_back(
+        {
+            [] (const pt::ptree &tree, const Job &job) {
+                return is_packet_having_type(
+                    tree, "LTE_PHY_Serv_Cell_Measurement"
+                );
+            },
+            extract_lte_phy_serv_cell_measurement
         }
     );
 
@@ -1571,6 +1584,52 @@ static void extract_lte_phy_pdsch_packet(
             (*g_output) << timestamp
                         << " $ LTE_PHY_PDSCH_Packet $ "
                         << result << std::endl;
+        }
+    );
+}
+
+static void extract_lte_phy_serv_cell_measurement(
+    pt::ptree &&tree, Job &&job) {
+    auto &&timestamp = get_packet_time_stamp(tree);
+
+    std::string result;
+    auto &&subpacket_lists = locate_subtree_with_attribute(
+        tree, "key", "Subpackets"
+    );
+    for (const auto subpacket_list : subpacket_lists) {
+        for (const auto &subpacket : subpacket_list->get_child("list")) {
+            enum class Status {unknown, primary, non_primary};
+            Status status = Status::unknown;
+            std::string rsrp;
+            for (const auto pair : subpacket.second.get_child("dict")) {
+                if (pair.second.get<std::string>("<xmlattr>.key")
+                    == "Serving Cell Index") {
+                    if (pair.second.get_value<std::string>() == "PCell") {
+                        status = Status::primary;
+                    } else {
+                        status = Status::non_primary;
+                    }
+                } else if (pair.second.get<std::string>("<xmlattr>.key")
+                    == "RSRP") {
+                    rsrp = pair.second.get_value<std::string>();
+                }
+                if (status != Status::unknown && !rsrp.empty()) {
+                    break;
+                }
+            }
+            if (status == Status::primary && !rsrp.empty()) {
+                result += timestamp;
+                result += " $ LTE_PHY_Serv_Cell_Measurement $ RSRP: ";
+                result += rsrp;
+                result += '\n';
+            }
+        }
+    }
+
+    insert_ordered_task(
+        job.job_num,
+        [result = std::move(result)] {
+            (*g_output) << result;
         }
     );
 }
